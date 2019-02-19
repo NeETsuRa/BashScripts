f364ac3add64b14ecb908975ef01064210f374d5
Nejc <dev.neetsu@gmail.com>
2019-02-19 08:51:30 +0100

    anonimize

--- Andro.sh
+++ Andro.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Default values
-PACKAGE="at.com.mbanking" #--> package can be found in the Manifest of the Application
-ACTIVITIE_NAME="at.com.mbanking.activities.Splash" #--> Activity Name can be found in the Manifest of the Application
-PROJECT_ROOT="/Users/nejc.ravnjak/Documents/Android Zips/android" #--> The Root folder of the Project
+PACKAGE="at.com.xxx" #--> package can be found in the Manifest of the Application
+ACTIVITIE_NAME="at.com.xxx.activities.Splash" #--> Activity Name can be found in the Manifest of the Application
+PROJECT_ROOT="/Users/android" #--> The Root folder of the Project
 PHONE_FOLDER="Phone" #--> Folder holding the build.gradle for Phone
 TABLET_FOLDER="Tablet" #--> Folder holding the build.gradle for Tablet
 
 #Build Tasks:
-UAT_PHONE="assembleUat2Debug" #--> gradle task for buildning the UAT Phone Release
-UAT_TABLET="assembleUat2Debug" #--> gradle task for buildning the UAT Tablet Release
-PROD_PHONE="assembleProdRelease" #--> gradle task for buildning the PROD Phone Release
-PROD_TABLET="assembleProdRelease" #--> gradle task for buildning the PROD Tablet Release
+UAT_PHONE="assembleDebug" #--> gradle task for buildning the UAT Phone Release
+UAT_TABLET="assembleDebug" #--> gradle task for buildning the UAT Tablet Release
+PROD_PHONE="assembleRelease" #--> gradle task for buildning the PROD Phone Release
+PROD_TABLET="assembleRelease" #--> gradle task for buildning the PROD Tablet Release
 
-PHONE_BUILD_OUT_DIR="/Users/nejc.ravnjak/Documents/Android\ Zips/android/Phone/build/outputs/apk" #--> The DIR where the Phone APK is stored (needet only for the Information log)
-TABLET_BUILD_OUT_DIR="/Users/nejc.ravnjak/Documents/Android\ Zips/-android/Tablet/build/outputs/apk" #--> The DIR where the Tablet APK is stored (needet only for the Information log)
+PHONE_BUILD_OUT_DIR="/Users/Phone/build/outputs/apk" #--> The DIR where the Phone APK is stored (needet only for the Information log)
+TABLET_BUILD_OUT_DIR="/Users/Tablet/build/outputs/apk" #--> The DIR where the Tablet APK is stored (needet only for the Information log)
 
-GIT_HUB_USER="nejc.ravnjak@accenture.com" #--> the GIT User for Release Taging
-GIT_HUB_RELEASE_LINK="https://api.github.com/repos/LeDominik/android/releases" #--> the GIT URL for Release Taging
+GIT_HUB_USER="mail@mai..com" #--> the GIT User for Release Taging
+GIT_HUB_RELEASE_LINK="https://api.github.com/repos/android/releases" #--> the GIT URL for Release Taging
 
 # Functions Used And some Usefull ones
 #
