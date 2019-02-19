d70ae0c33bb7f09c7e9e9710af75297ef0590073
Nejc <42175966+NeETsuRa@users.noreply.github.com>
2019-02-20 00:11:05 +0100

    remove Config parameters
    Remove the config Parameters from the base script

--- Android/Andro.sh
+++ Android/Andro.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-
-# Default values
-PACKAGE="at.com.xxx" #--> package can be found in the Manifest of the Application
-ACTIVITIE_NAME="at.com.xxx.activities.Splash" #--> Activity Name can be found in the Manifest of the Application
-PROJECT_ROOT="/Users/android" #--> The Root folder of the Project
-PHONE_FOLDER="Phone" #--> Folder holding the build.gradle for Phone
-TABLET_FOLDER="Tablet" #--> Folder holding the build.gradle for Tablet
-
-#Build Tasks:
-UAT_PHONE="assembleDebug" #--> gradle task for buildning the UAT Phone Release
-UAT_TABLET="assembleDebug" #--> gradle task for buildning the UAT Tablet Release
-PROD_PHONE="assembleRelease" #--> gradle task for buildning the PROD Phone Release
-PROD_TABLET="assembleRelease" #--> gradle task for buildning the PROD Tablet Release
-
-PHONE_BUILD_OUT_DIR="/Users/Phone/build/outputs/apk" #--> The DIR where the Phone APK is stored (needet only for the Information log)
-TABLET_BUILD_OUT_DIR="/Users/Tablet/build/outputs/apk" #--> The DIR where the Tablet APK is stored (needet only for the Information log)
-
-GIT_HUB_USER="mail@mai..com" #--> the GIT User for Release Taging
-GIT_HUB_RELEASE_LINK="https://api.github.com/repos/android/releases" #--> the GIT URL for Release Taging
-
 # Functions Used And some Usefull ones
 #
 # List all Devices
@@ -51,6 +31,8 @@ BLUE='\033[0;34m'
 BOLD_BLUE='\033[1;34m'
 COLOR_RESET='\033[0m'
 
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+source "${PROJECT_DIR}/Andro-config.sh"
 REPOSITORY=""
 PROGNAME="$(basename $0)"
 ROOT=$(pwd)
